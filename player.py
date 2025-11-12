5e570ae94e7ee1cf7360b6ecafccdf397f63f914
JustSnoww <98555747+JustSnoww@users.noreply.github.com>
2025-11-12 14:10:29 -0600

    updated Player class to be able to rotate using a and d keys on the keyboard added PLAYER_TURN_SPEED to constants

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS 
 from constants import LINE_WIDTH
+from constants import PLAYER_TURN_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -20,4 +21,15 @@ class Player(CircleShape):
         color = "white"
         line_width = LINE_WIDTH
         pygame.draw.polygon(screen, color, self.triangle(), line_width)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
             
+        if keys[pygame.K_d]:
+            self.rotate(dt)
